d0610a4361a3f658918f34258a1d11cbedfd9423
Corrin Barnes <corrinbarnes@gmail.com>
2017-11-30 21:33:48 -0500

    fish is bumpin

--- fish.js
+++ fish.js
@@ -1,3 +1,8 @@
+const MAX_X = 1024;
+const MAX_Y = 768;
+const MIN_X = 0;
+const MIN_Y = 0;
+
 class Fish {
 
   constructor(element) {
@@ -6,15 +11,24 @@ class Fish {
     this.y = 0;
     this.speedX = 1;
     this.speedY = 1;
+    this.width = this.element.offsetWidth;
+    this.height = this.element.offsetHeight;
   }
 
   update() {
+    const fishRight = this.x + this.width;
+    const fishBottom = this.y + this.height;
+    if (fishRight >= MAX_X || this.x < MIN_X ) {
+      this.speedX *= -1;
+    }
+    if (fishBottom >= MAX_Y || this.y < MIN_Y) {
+      this.speedY *= -1;
+    }
     this.x += this.speedX;
     this.y += this.speedY;
   }
 
   display() {
-    console.log('display');
     this.element.style.left = this.x;
     this.element.style.top = this.y;
   }
