5342246a039d31e10c996de4f56ebfd469498373
Corrin Barnes <corrinbarnes@gmail.com>
2017-12-05 21:13:21 -0500

    using sine wave for fish movement

--- fish.js
+++ fish.js
@@ -3,14 +3,22 @@ const MAX_Y = 768;
 const MIN_X = 0;
 const MIN_Y = 0;
 
+Math.tau = 2 * Math.PI;
+
+sineWave = (x, amplitude, frequency, depth) => {
+  return (amplitude * Math.sin(Math.tau * frequency * x)) + depth;
+}
+
 class Fish {
 
   constructor(element) {
     this.element = element;
     this.x = 0;
+    this.depth = 500;
     this.y = 0;
+    this.amplitude = 20;
+    this.frequency = 0.005;
     this.speedX = 1;
-    this.speedY = 1;
     this.width = this.element.offsetWidth;
     this.height = this.element.offsetHeight;
     this.orientation = 1;
@@ -23,13 +31,14 @@ class Fish {
       this.speedX *= -1;
       this.orientation *= -1;
     }
-    if (fishBottom >= MAX_Y || this.y < MIN_Y) {
-      this.speedY *= -1;
-    }
 
     this.x += this.speedX;
-    this.y += this.speedY;
+    this.y = sinWave(this.x, this.amplitude, this.frequency, this.depth);
+
+    this.y = Math.max(MIN_Y, this.y);
+    this.y = Math.min(MAX_Y, this.y);
   }
+
 
   display() {
     this.element.style.left = this.x;
